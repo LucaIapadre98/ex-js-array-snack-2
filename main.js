e372d8e19752cb5ab0b7b2b4ef3968e454b5e5df
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-07-29 10:21:56 +0200

    snack 5

--- main.js
+++ main.js
@@ -103,4 +103,21 @@ const agesSum = ages.reduce((acc, age) => acc + age, 0 );
 console.log(agesSum);
 
 const averageAge = (agesSum / ages.length).toFixed(2);
-console.log(averageAge);
+console.log(`L'età media degli autori è: ${averageAge}`);
+
+// Usando la l'API https://boolean-spec-frontend.vercel.app/freetestapi/books/{id} usa la combinazione di .map() e Promise.all(), 
+// per creare una funzione (getBooks) che a partire da un array di id (ids), ritorna una promise che risolve un array di libri (books).
+// Testala con l’array [2, 13, 7, 21, 19] .
+
+const ids = [2, 13, 7, 21, 19];
+
+async function getBooks(ids) {
+    const baseUrl = 'https://boolean-spec-frontend.vercel.app/freetestapi/books/';
+
+    const promise = ids.map(id => fetch(`${baseUrl}${id}`).then(response => response.json()));     // promise è un array di promesse, ognuna delle quali recupera un libro dall'API
+
+    const books = await Promise.all(promise);                                               // books è il risultato di tutte le promesse risolte
+    return books;                                                                  // ritorna books
+};
+
+getBooks(ids).then(books => console.log(books)).catch(error => console.error('Error fetching books:', error));
